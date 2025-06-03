d0954fb94f85248e1811992a0490b35b5caba9b2
advaithsuryaP <padvaithsurya@gmail.com>
2025-06-03 00:26:24 -0400

    Refactor app routing and add header and shell components
    - Updated app routing to include a shell component as a parent for to-do and task explorer routes.
    - Created HeaderComponent and ShellComponent with basic templates and styles.
    - Added unit tests for both HeaderComponent and ShellComponent.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,22 +4,28 @@ import { AuthGuard } from './core/guards/auth.guard';
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'to-do',
         pathMatch: 'full',
+        redirectTo: 'to-do',
     },
     {
         path: 'login',
         loadChildren: () => import('./auth/auth.routes'),
     },
     {
-        path: 'to-do',
+        path: '',
         canActivate: [AuthGuard],
-        loadComponent: () => import('./todo/todo.component'),
-    },
-    {
-        path: 'task-explorer',
-        canActivate: [AuthGuard],
-        loadComponent: () => import('./task-explorer/task-explorer.component'),
+        loadComponent: () => import('./core/components/shell/shell.component'),
+        children: [
+            {
+                path: 'to-do',
+                loadComponent: () => import('./todo/todo.component'),
+            },
+            {
+                path: 'task-explorer',
+                loadComponent: () =>
+                    import('./task-explorer/task-explorer.component'),
+            },
+        ],
     },
     {
         path: '**',
